296e661c73b1f6fd2dc651b5d6eca5305b5a657c
george-sarji <georgesarji@gmail.com>
2022-12-17 10:14:58 +0200

    Start implementing symbol table

--- Assignments/hw3/SymbolTable.cpp
+++ Assignments/hw3/SymbolTable.cpp
@@ -1,2 +1,25 @@
 #include "SymbolTable.h"
 
+bool SymbolTable::isDefined(string symbol)
+{
+    // We need to go over all of the scopes and check in each scope.
+    for (ScopeTable* scope : scopes)
+    {
+        // Check in the current scope.
+        if (scope->isDefined(symbol))
+        {
+            return true;
+        }
+    }
+    // We haven't found it in any of the scopes.
+    // Exit.
+    return false;
+}
+
+void SymbolTable::addNewSymbol(string name, string type)
+{
+    // Let's go into the top scope (beginning of the vector.)
+    ScopeTable* currentScope = scopes.front();
+    // Let's add a new row into the scope table.
+    currentScope->addRow(name, type);
+}
