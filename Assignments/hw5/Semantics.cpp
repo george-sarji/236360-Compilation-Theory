2402d36d70d666e7e0c92aa3916d4093e2617adc
george-sarji <georgesarji@gmail.com>
2023-01-24 13:07:01 +0200

    add missing register declaration

--- Assignments/hw5/Semantics.cpp
+++ Assignments/hw5/Semantics.cpp
@@ -207,7 +207,7 @@ Exp::Exp(Exp *left, Node *op, Exp *right, bool isRelop)
                 }
             }
             // Emit the comparison.
-            buffer.emit("%" + registerName + " = icmp " + icmpRelop + " " + (isSigned ? "i32" : "i8") + " %" + leftRegister + ", " + rightRegister);
+            buffer.emit("%" + registerName + " = icmp " + icmpRelop + " " + (isSigned ? "i32" : "i8") + " %" + leftRegister + ", %" + rightRegister);
             // TODO: What does this do?
         }
         else
